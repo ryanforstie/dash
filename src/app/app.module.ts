2e9cc40a222690e84630bfb2d61bc1296b9354ba
Forstie <Ryan.Forstie1@T-Mobile.com>
2019-12-16 16:25:50 -0800

    Initial setup of sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSliderModule } from "@angular/material/slider";
+import { SidenavComponent } from "./sidenav/sidenav.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSliderModule],
+  declarations: [AppComponent, SidenavComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule],
   providers: [],
   bootstrap: [AppComponent]
 })
