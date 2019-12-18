c1cdfb6d22c2db6bb67223480088b49e12568c83
Forstie <Ryan.Forstie1@T-Mobile.com>
2019-12-17 16:41:36 -0800

    Add dashboard route as default home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSidenavModule } from "@angular/material/sidenav";
+import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
 import { SidenavComponent } from "./sidenav/sidenav.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @NgModule({
-  declarations: [AppComponent, SidenavComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatSidenavModule],
+  declarations: [AppComponent, SidenavComponent, DashboardComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
